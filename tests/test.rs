6e3ff82970cdd4b8a4cb14ae97316b1590d06787
Jie Yang <jie.yang@nanchao.org>
2020-02-11 11:40:03 +0800

    add trait obj test case

--- tests/test.rs
+++ tests/test.rs
@@ -13,20 +13,21 @@ fn bad_add(a: i32, b: i32) -> i32 {
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
-    use std::rc::Rc;
+    use std::io::Error;
     use std::mem;
+    use std::rc::Rc;
 
     #[test]
     fn test_add() {
         assert_eq!(add(1, 2), 3);
     }
 
-   // #[test]
-   // fn test_bad_add() {
-   //     // This assert would fire and test will fail.
-   //     // Please note, that private functions can be tested too!
-   //     assert_eq!(bad_add(1, 2), 3);
-   // }
+    // #[test]
+    // fn test_bad_add() {
+    //     // This assert would fire and test will fail.
+    //     // Please note, that private functions can be tested too!
+    //     assert_eq!(bad_add(1, 2), 3);
+    // }
 
     #[test]
     fn test_option_iter() {
@@ -46,7 +47,10 @@ mod tests {
     #[test]
     fn test_default_trait() {
         let options: SomeOptions = Default::default();
-        let option1 = SomeOptions {foo: 42, ..Default::default()};
+        let option1 = SomeOptions {
+            foo: 42,
+            ..Default::default()
+        };
         assert_eq!(option1.foo, 42);
     }
 
@@ -72,26 +76,95 @@ mod tests {
 
     enum MyEnum {
         A { name: String, x: u8 },
-        B { name: String }
+        B { name: String },
     }
 
     fn a_to_b(e: &mut MyEnum) {
-        *e = if let MyEnum::A { ref mut name, x: 0} = *e {
-            MyEnum::B { name: mem::replace(name, String::new()) }
+        *e = if let MyEnum::A { ref mut name, x: 0 } = *e {
+            MyEnum::B {
+                name: mem::replace(name, String::new()),
+            }
         } else {
-            return
+            return;
         }
     }
 
     #[test]
     fn test_mem_replace() {
-        let mut a = MyEnum::A { name: String::from("tope"), x: 0 };
+        let mut a = MyEnum::A {
+            name: String::from("tope"),
+            x: 0,
+        };
 
         a_to_b(&mut a);
 
         match a {
-            MyEnum::A {name:_, x:_} => assert!(false),
-            MyEnum::B {name} => assert_eq!(name, String::from("tope"))
+            MyEnum::A { name: _, x: _ } => assert!(false),
+            MyEnum::B { name } => assert_eq!(name, String::from("tope")),
+        }
+    }
+    trait FileReader {
+        fn read_all(&self) -> Result<String, Error>;
+    }
+
+    impl FileReader for str {
+        fn read_all(&self) -> Result<String, Error> {
+            use std::fs::File;
+            use std::io::prelude::*;
+
+            let mut contents = String::new();
+            let mut f = File::open(self)?;
+            f.read_to_string(&mut contents)?;
+            Ok(contents)
+        }
+    }
+
+    impl FileReader for Option<String> {
+        fn read_all(&self) -> Result<String, Error> {
+            use std::io::ErrorKind;
+            match *self {
+                Option::None => Err(Error::new(ErrorKind::NotFound, "No such file or directory")),
+                Option::Some(ref s) => s.read_all(),
+            }
+        }
+    }
+
+    trait FileReaderIter: Iterator<Item = String> {
+        fn read_nth(&mut self, pos: usize) -> Result<String, Error> {
+            self.nth(pos).read_all()
+        }
+    }
+
+    struct MyTest {
+        inner: Vec<String>,
+    }
+    impl Iterator for MyTest {
+        type Item = String;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.inner.pop()
+        }
+    }
+
+    //impl FileReaderIter for Args {}
+    impl FileReaderIter for MyTest {}
+
+    fn assert_check_content<T: AsRef<str>>(t: T) {
+        assert_eq!(t.as_ref(), "hello rust\n");
+    }
+    #[test]
+    fn test_trait_obj() {
+        let mut my_test = MyTest {
+            inner: vec!["./tests/test_trait.txt".to_string()],
+        };
+        let trait_obj: &mut dyn FileReaderIter = &mut my_test;
+
+        if let Err(err) = trait_obj.read_nth(0).and_then(|content| {
+            assert_check_content(content);
+            Ok(())
+        }) {
+            println!("err is {:?}", err);
+            assert!(false);
         }
     }
 }
